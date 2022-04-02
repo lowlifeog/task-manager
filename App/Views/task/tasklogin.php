9d1aa6a84c302ca5883e9bc7c59ddcec5ba60983
Nikita Zavyalov <kitushakit@gmail.com>
2022-04-02 18:43:10 +0300

    add login view

---
+++ App/Views/task/tasklogin.php
@@ -0,0 +1,25 @@
+<?php require_once  ROOT . '/app/views/common/header.php'; ?>
+    <h2 class="my-2">Login</h2>
+    <?php if ($data['errors']): ?>
+    <div class="alert alert-danger" role="alert">
+        <ul>
+            <?php foreach ($data['errors'] as $error) : ?>
+                <li><?php echo $error; ?></li>
+            <?php endforeach; ?>
+        </ul>
+    </div>
+    <?php endif; ?>
+    <form action="#" method="post" enctype="multipart/form-data">
+        <div class="form-group">
+            <label for="inputLogin" class="form-label fw-bold">Login</label>
+            <input type="text" class="form-control" id="inputLogin"  name="login">
+        </div>
+        <div class="form-group">
+            <label for="inputPass" class="form-label fw-bold">Pass</label>
+            <input type="text" class="form-control" id="inputPass"  name="pass">
+        </div>
+        <div class="form-group my-3">
+            <button type="submit" class="btn btn-primary" value="Login" name="submit">Login</button>
+        </div>
+    </form>
+<?php require_once  ROOT . '/app/views/common/footer.php'; ?>
