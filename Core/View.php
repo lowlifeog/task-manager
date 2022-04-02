739434ee3239d4510901cf99be168899c919d373
Nikita Zavyalov <kitushakit@gmail.com>
2022-04-03 01:51:29 +0300

    change view controller

--- Core/View.php
+++ Core/View.php
@@ -2,21 +2,15 @@
 
 namespace core;
 
+use App\Controllers\TaskController;
+
 class View {
 
     public static function render(string $path, array $data = []) {
 
         $file = ROOT . '/app/views/' . $path . '.php';
 
-        if (!file_exists($file)) {
-            throw new \ErrorException("$file cannot be found");
-        }
-
-        if (!empty($data)) {
-            foreach ($data as $key => $value) {
-                $$key = $value;
-            }
-        }
+        $data["logged"] = TaskController::isLogged();
 
         require_once  $file;
     }
