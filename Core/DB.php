7c595dcb9c4713c19ac3608ec19b093a22af2365
Nikita Zavyalov <kitushakit@gmail.com>
2022-04-03 16:15:08 +0300

    Update DB.php

--- Core/DB.php
+++ Core/DB.php
@@ -6,13 +6,11 @@ use PDO;
 
 class DB {
 
-    
-
     public static function connect(){
         $cs = self::$connect_settings;
 
-        $params = "mysql:host={$cs['host']};dbname={$cs['dbname']};charset=utf8";
-        $db = new PDO($params, $cs['user'], $cs['pass']);
+        $dsn = "mysql:host={$cs['host']};dbname={$cs['dbname']};charset=utf8";
+        $db = new PDO($dsn, $cs['user'], $cs['pass'], [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
 
         return $db;
     }
