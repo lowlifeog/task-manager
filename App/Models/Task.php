14d9836115d6eaee410ba84b868c50de3b1d8e99
Nikita Zavyalov <kitushakit@gmail.com>
2022-04-03 16:18:19 +0300

    add editTask, getTask, checkUser

--- App/Models/Task.php
+++ App/Models/Task.php
@@ -7,13 +7,20 @@ use PDO;
 
 class Task {
 
-    public static function getTasks(){
+    public static function getTasks($page = 1, $limit = 3, $sort = 'task_user', $order = 'ASC'){
         $db = DB::connect();
 
+        $offset = $limit * ($page - 1);
+
         $query = "SELECT * "
-                . "FROM task ";
+                . "FROM task "
+                . "ORDER BY $sort $order "
+                . "LIMIT $limit "
+                . "OFFSET $offset";;
 
         $result = $db->query($query);
+
+        $tasks = array();
 
         foreach ($result as $row) {
             $tasks[] = array(
@@ -22,13 +29,52 @@ class Task {
                 'email' => $row['task_email'],
                 'text' => $row['task_text'],
                 'status' => $row['task_status'],
+                'edit' => $row['task_edit']
             );
         }
 
         return $tasks;
     }
 
-    public static function addTask($name, $email, $text) {
+    public static function getTask($id){
+        $db = DB::connect();
+
+
+        $query = "SELECT * "
+                . "FROM task "
+                . "WHERE "
+                . "task_id = $id";
+
+        $result = $db->query($query);
+
+        $row = $result->fetch(PDO::FETCH_ASSOC);
+
+        $task = array(
+            'id' => $row['task_id'],
+            'name' => $row['task_name'],
+            'email' => $row['task_email'],
+            'text' => $row['task_text'],
+            'status' => $row['task_status'],
+            'edit' => $row['task_edit']
+        );
+
+        return $task;
+    }
+
+    public static function getTotalTasks(){
+        $db = DB::connect();
+
+        $query = "SELECT COUNT(*) AS count "
+                . "FROM task";
+
+        $result = $db->query($query);
+
+        $result = $result->fetch(PDO::FETCH_ASSOC);
+
+        return $result['count'];
+    }
+
+    public static function addTask($task){
 
         $db = DB::connect();
 
@@ -38,10 +84,54 @@ class Task {
                 . "task_text = :text";
 
         $result = $db->prepare($query);
-        $result->bindParam(':name', $name, PDO::PARAM_STR);
-        $result->bindParam(':email', $email, PDO::PARAM_STR);
-        $result->bindParam(':text', $text, PDO::PARAM_STR);
+        $result->bindParam(':name', $task['name'], PDO::PARAM_STR);
+        $result->bindParam(':email', $task['email'], PDO::PARAM_STR);
+        $result->bindParam(':text', $task['text'], PDO::PARAM_STR);
 
         return $result->execute();
+    }
+
+    public static function editTask($data){
+
+        $db = DB::connect();
+
+        $query = "UPDATE task SET "
+                . "task_text = :text, "
+                . "task_status = :status, "
+                . "task_edit = :edit "
+                . "WHERE "
+                . "task_id = :id";
+
+
+        $result = $db->prepare($query);
+        $result->bindParam(':text', $data['text'], PDO::PARAM_STR);
+        $result->bindParam(':status', $data['status'], PDO::PARAM_INT);
+        $result->bindParam(':edit', $data['edit'], PDO::PARAM_INT);
+        $result->bindParam(':id', $data['id'], PDO::PARAM_INT);
+
+        return $result->execute();
+
+    }
+
+    public static function checkUser($user){
+
+        $db = DB::connect();
+
+        $query = "SELECT * FROM user WHERE "
+                . "login = :login AND "
+                . "pass = :pass";
+
+        $result = $db->prepare($query);
+        $result->bindParam(':login', $user['login'], PDO::PARAM_STR);
+        $result->bindParam(':pass', $user['pass'], PDO::PARAM_STR);
+
+        $result->execute();
+
+        $data = $result->fetch(PDO::FETCH_ASSOC);
+        if ($data){
+            return $data['user_id'];
+        }
+
+        return false;
     }
 }
