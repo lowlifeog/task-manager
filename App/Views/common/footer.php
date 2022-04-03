203084ae353ca62208a17463bad3cb5120189247
Nikita Zavyalov <kitushakit@gmail.com>
2022-04-03 17:06:17 +0300

    script replace

--- App/Views/common/footer.php
+++ App/Views/common/footer.php
@@ -1,5 +1,5 @@
         </div>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-        <script src="/src/script.js"></script>
+        <script src="/src/js/script.js"></script>
     </body>
 </html>
